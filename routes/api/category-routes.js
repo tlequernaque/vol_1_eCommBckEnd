65cff5c0306e7f102e8e403090c06cb50d97b1a4
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-27 01:16:10 -0400

    changed code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,53 +4,63 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  try {
-    const categoryData = Category.findAll({
-      attributes:[
-        'id',
-        'category_name',
-      ],
-      include: [
-        {
-          model: Product,
-          attributes: ['id','product_name', 'price', 'stock', 'category_id'],
-        },
-      ],
-    });
-    res.status(200).json(categoryData);
-  } catch (err){
-    res.status(500).json(err);
-  }
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name',
+    ],
+    include:[
+      {
+        model: Product,
+        attributes: ['id','product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+    .then()
+    .catch()
+  //   res.status(200).json(categoryData);
+  // } catch (err){
+  //   res.status(500).json(err);
+  // }
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
-  try {
-    const categoryData = Category.findOne(req.params.id, {
-      attributes:[
-        'id',
-        'category_name',
-      ],
-      include: [
-        {
-          model: Product,
-          attributes: ['id','product_name', 'price', 'stock', 'category_id'],
-        },
-      ],
-    });
-    if (!categoryData){
-      res.status(404).json({ message: 'No catedory found with that id!'})
-    }
-    res.status(200).json(categoryData);
-  } catch (err){
-    res.status(500).json(err);
-  }
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name',
+    ],
+    include:[
+      {
+        model: Product,
+        attributes: ['id','product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+    .then()
+    .catch()
+
+  //   if (!categoryData){
+  //     res.status(404).json({ message: 'No category found with that id!'})
+  //   }
+  //   res.status(200).json(categoryData);
+  // } catch (err){
+  //   res.status(500).json(err);
+  // }
+
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+    
+  })
   // create a new category
 });
 
