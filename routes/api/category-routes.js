28cd8e74c99cd34bb4c4f02b1df7f8eb7c5a322b
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-28 18:40:26 -0400

    changed code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,24 +4,32 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  Category.findAll({
-    attributes: [
-      'id',
-      'category_name',
-    ],
-    include:[
-      {
-        model: Product,
-        attributes: ['id','product_name', 'price', 'stock', 'category_id'],
-      },
-    ],
-  })
-    .then()
-    .catch()
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // Category.findAll({
+  //   attributes: [
+  //     'id',
+  //     'category_name',
+  //   ],
+  //   include:[
+  //     {
+  //       model: Product,
+  //       attributes: ['id','product_name', 'price', 'stock', 'category_id'],
+  //     },
+  //   ],
+  // })
+  //   .then()
+  //   .catch()
   //   res.status(200).json(categoryData);
   // } catch (err){
   //   res.status(500).json(err);
   // }
+  
   // find all categories
   // be sure to include its associated Products
 });
