eea1f54dac5accf450284bf9fc189d358f2830b8
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-29 00:12:42 -0400

    completed api tag-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No Category found with this id!' });
       return;
     }
 
@@ -50,7 +50,7 @@ router.put('/:id', async (req, res) => {
       }
     });
     if(!categoryData){
-      res.status(404).json({message: 'No category found with this id!'});
+      res.status(404).json({message: 'No Category found with this id!'});
       return;
     }
     res.status(200).json(categoryData);
@@ -68,7 +68,7 @@ router.delete('/:id', async (req, res) => {
       }
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No Category found with this id!' });
       return;
     }
 
